f476b8f5bb72dc3328632faf7c276d4fae2a5102
TTQN <91316891+TTQN@users.noreply.github.com>
2023-06-26 12:26:32 +0700

    Delete src directory

--- src/routes/payment.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const paymentController = require("../app/controllers/paymentController");
-
-//newsController.index
-router.get("/add", paymentController.payment);
-
-module.exports = router;
